6ffa62d08b2fcfbcf1f1e0a2ecbae15a880b2015
ABTO69 <abdo.formal99@gmail.com>
2024-10-18 16:53:50 +0300

    service cleanup

--- Ibdal.Api/Program.cs
+++ Ibdal.Api/Program.cs
@@ -1,18 +1,8 @@
 using Ibdal.Api;
-using Identity.Mongo;
-using Microsoft.AspNetCore.Identity;
 
 var builder = WebApplication.CreateBuilder(args);
 
-builder.Services.AddMongoDb<AppDbContext>(
-    connectionString: builder.Configuration["MongoDb:ConnectionString"]!,
-    databaseName: builder.Configuration["MongoDb:DatabaseName"]!);
-
-builder.Services.AddIdentity<IdentityUser, IdentityRole>()
-    .AddMongoStores()
-    .AddDefaultTokenProviders();
-
-builder.Services.AddControllers();
+builder.ConfigureServices();
 
 var app = builder.Build();
 
---
+++ Ibdal.Api/RegisterServices.cs
@@ -0,0 +1,21 @@
+﻿using Identity.Mongo;
+using Microsoft.AspNetCore.Identity;
+
+namespace Ibdal.Api;
+
+public static class RegisterServices
+{
+    public static void ConfigureServices(this WebApplicationBuilder builder)
+    {
+        builder.Services.AddControllers();
+        builder.Services.AddEndpointsApiExplorer();
+        
+        builder.Services.AddMongoDb<AppDbContext>(
+            connectionString: builder.Configuration["MongoDb:ConnectionString"]!,
+            databaseName: builder.Configuration["MongoDb:DatabaseName"]!);
+
+        builder.Services.AddIdentity<IdentityUser, IdentityRole>()
+            .AddMongoStores()
+            .AddDefaultTokenProviders();
+    }
+}
